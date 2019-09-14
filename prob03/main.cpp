4608b0b32a97cc3b276cdeb54e6f216d2b406fde
riften <riften@cs104u-5d.acad.fullerton.edu>
2019-09-14 14:56:10 -0700

    Created program to calculate sales prediction

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,23 @@
 // Sales prediction
+
+#include <iostream>
+
+int main()
+{
+  double percent_of_sales_increased = 18;
+  int total_sales_2018 = 2103419277; // Total sales for last year
+  long long int amount_of_sales_increased;
+  long long int prediction; // Prediction of next year's sales
+
+
+  // Calculates prediction of sales for next year
+  amount_of_sales_increased = total_sales_2018 * (percent_of_sales_increased / 100);
+  prediction = amount_of_sales_increased + total_sales_2018;
+  std::cout << prediction << std::endl;
+
+  // Prints total of sales for 2018 and prediction of sales for next year
+  std::cout << "Last year's sales were $" << total_sales_2018 << std::endl;
+  std::cout << "This year's sales prediction: $" << prediction << std::endl;
+
+  return 0;
+}
